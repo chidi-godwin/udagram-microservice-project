929ad740a6b243502e3172a9df54e2c975b109d3
chidi-godwin <chidieberen1998@gmail.com>
2021-09-15 12:32:28 +0100

    repackage deployment

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_USERNAME=postgres
+export POSTGRES_HOST=postgres.c42anbngt4we.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=gnc-microservice-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=nanodegree
+export JWT_SECRET=whatisasecretthatwehavetokeep
+export URL=http://localhost:8100
