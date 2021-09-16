cb89f3322950b5b162fbd97f4e3ed068eabe7186
chidi-godwin <chidieberen1998@gmail.com>
2021-09-16 06:37:10 +0100

    add new hosts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://ac18f7ccf15a14307a9abf58b7bf5c38-1349447405.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://affa3d7b283c8478c8f076b16feea7aa-1893316979.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
